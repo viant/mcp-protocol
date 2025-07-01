ffdad25e66d3b9a615a8e7d6ac367f63710bf7c9
adranwit <adrianwit@gmail.com>
2025-07-01 07:01:40 -0700

    upgraded protocol to version 2025-06-18

--- schema/const.go
+++ schema/const.go
@@ -2,7 +2,9 @@ package schema
 
 const (
 	LatestProtocolVersion   = "2025-06-18"
-	TokenProgressContextKey = tokenProgress("TokenProgressContextKey")
+	TokenProgressContextKey = tokenProgress("TokenProgress")
+	McpSessionContextKey    = mcpSessionId("MCPSessionId")
 )
 
 type tokenProgress string
+type mcpSessionId string
